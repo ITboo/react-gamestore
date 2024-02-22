847f8285e86a92d7c9e5ad381355b46577d67c92
ITboo <annaacdc@tut.by>
2024-02-22 21:09:38 +0300

    init: start with Vite

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import FilterableList from './component/FilterableList'
+
+function App() {
+  return (
+    <FilterableList />
+
+  )
+}
+
+export default App
---
+++ src/component/FilterableList/index.jsx
@@ -0,0 +1,5 @@
+export default function FilterableList() {
+  return (
+    <div>FilterableList</div>
+  )
+}
